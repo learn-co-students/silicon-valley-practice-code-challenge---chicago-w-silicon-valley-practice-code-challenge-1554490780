066bbe5c160f26f1897e4a95aeef0d083de00145
Jackson Robbins <jackrobbins1@gmail.com>
2019-04-05 15:07:53 -0500

    code for my first coding challenge

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,48 @@
+require "pry"
+
 class Startup
 
+  # class /////////////
+  @@all = []
+  @@domains = []
+
+  def self.all
+    @@all
+  end
+
+  def self.domains
+    @@domains
+  end
+
+  def self.find_by_founder(name)
+    self.all.find { |startup|
+      startup.founder == name
+    }
+  end
+
+  # instance //////////
+  attr_reader :name, :founder, :domain
+
+  def initialize(name, founder, domain)
+    # class //////////////////
+    @@all << self
+    @@domains << domain
+    # instance ///////////////
+    @name = name
+    @founder = founder
+    @domain = domain
+  end
+
+  def pivot(new_domain)
+    @domain = new_domain
+  end
+
 end
+
+
+biz = Startup.new("DaBiz", "Jack", "clothing")
+biz2 = Startup.new("DaBiz2", "Joe", "ceramics")
+biz3 = Startup.new("DaBiz3", "Moe", "dogs")
+
+binding.pry
+"sup"
